b8a56dc44f9ebbfc6570197be3bb6c652e21e7d8
just-paja <just.paja@gmail.com>
2016-12-04 13:00:22 +0100

    Add Lector model str method

--- api/models/lector.py
+++ api/models/lector.py
@@ -8,3 +8,7 @@ class Lector(Base):
 
     name = models.CharField(max_length=127)
     about = models.TextField()
+
+    def __str__(self):
+        """Return name as string representation."""
+        return self.name
