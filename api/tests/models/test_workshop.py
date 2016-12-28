e106a3724197f9615d759c915731751de26a6e5a
Petr Dlouhý <petr.dlouhy@email.cz>
2016-12-28 14:03:52 +0100

    Make CapacityMixin and use it for Workshop model.
    CapacityMixin holds also methods for determining if capacity is exceeded.
    This is tested at Workshop model.

--- api/tests/models/test_workshop.py
+++ api/tests/models/test_workshop.py
@@ -1,6 +1,13 @@
 """Tests for workshop model."""
+import datetime
+
+from api.models import Workshop
+
 from django.test import TestCase
-from api.models.workshop import Workshop
+
+from freezegun import freeze_time
+
+from model_mommy import mommy
 
 
 class WorkshopTest(TestCase):
@@ -10,3 +17,39 @@ class WorkshopTest(TestCase):
         """Test that workshop turns to string properly."""
         entry = Workshop(name="Foo Workshop")
         self.assertEqual(str(entry), 'Foo Workshop')
+
+    @freeze_time("2017-2-1")
+    def test_capacity(self):
+        reservation = mommy.make(
+            'api.Reservation',
+            ends_at=datetime.datetime(year=2017, month=3, day=1),
+            workshop_price__workshop__capacity=1,
+            order__paid=False,
+        )
+        workshop = reservation.workshop_price.workshop
+        self.assertEqual(workshop.number_of_reservations(), 1)
+        self.assertEqual(workshop.has_free_capacity(), False)
+
+    @freeze_time("2017-2-1")
+    def test_capacity_after_reservation(self):
+        reservation = mommy.make(
+            'api.Reservation',
+            ends_at=datetime.datetime(year=2017, month=1, day=1),
+            workshop_price__workshop__capacity=1,
+            order__paid=False,
+        )
+        workshop = reservation.workshop_price.workshop
+        self.assertEqual(workshop.number_of_reservations(), 0)
+        self.assertEqual(workshop.has_free_capacity(), True)
+
+    @freeze_time("2017-2-1")
+    def test_capacity_paid(self):
+        reservation = mommy.make(
+            'api.Reservation',
+            ends_at=datetime.datetime(year=2017, month=1, day=1),
+            workshop_price__workshop__capacity=1,
+            order__paid=True,
+        )
+        workshop = reservation.workshop_price.workshop
+        self.assertEqual(workshop.number_of_reservations(), 1)
+        self.assertEqual(workshop.has_free_capacity(), False)
