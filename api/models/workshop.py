71d38276039676e4a5d7b70b522bcf02d12b499f
Pavel Žák <just.paja@gmail.com>
2016-12-30 19:36:04 +0100

    Add verbose names and descriptions
    Fixes #5

--- api/models/workshop.py
+++ api/models/workshop.py
@@ -13,13 +13,20 @@ class Workshop(Base):
     """Stores workshops."""
 
     name = models.CharField(max_length=127)
-    desc = models.TextField()
+    desc = models.TextField(
+        verbose_name=_("Description"),
+        help_text=_("Describe what is the goal of the workshop, props necessary, etc."),
+    )
     difficulty = models.ForeignKey(
         WorkshopDifficulty,
         verbose_name=_("Difficulty"),
     )
     visibility = models.PositiveIntegerField(choices=VISIBILITY_CHOICES)
-    capacity = models.PositiveIntegerField(default=12)
+    capacity = models.PositiveIntegerField(
+        default=12,
+        verbose_name=_("Capacity"),
+        help_text=_("How many people can fit in?"),
+    )
     lectors = models.ManyToManyField(Lector, related_name='workshops', through=WorkshopLector)
 
     def __str__(self):
