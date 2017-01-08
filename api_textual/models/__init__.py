c0203fc3cf1514b7a4c66664511c41f89797ca5b
Petr Dlouhý <petr.dlouhy@email.cz>
2017-01-08 22:34:49 +0100

    add TravelingTip model

--- api_textual/models/__init__.py
+++ api_textual/models/__init__.py
@@ -2,12 +2,16 @@
 
 from .text import Text
 from .textPhoto import TextPhoto
+from .travelingTip import TravelingTip
+from .travelingTipPhoto import TravelingTipPhoto
 from .workshopLocation import WorkshopLocation
 from .workshopLocationPhoto import WorkshopLocationPhoto
 
 __all__ = (
     Text,
     TextPhoto,
+    TravelingTip,
+    TravelingTipPhoto,
     WorkshopLocation,
     WorkshopLocationPhoto,
 )
