4841f1445df2fc7c467c13bcfc671a7037b82397
Petr Dlouhý <petr.dlouhy@email.cz>
2017-01-31 14:50:18 +0100

    rename serializers -> years

--- improtreskApi/rest_router.py
+++ improtreskApi/rest_router.py
@@ -1,9 +1,9 @@
 from rest_framework import routers
 
-from api.rest import serializers
+from api.rest import years
 from api_textual.rest import news, texts
 
 router = routers.DefaultRouter()
 router.register(r'news', news.NewsViewSet)
 router.register(r'texts', texts.TextViewSet)
-router.register(r'years', serializers.YearSet)
+router.register(r'years', years.YearViewSet)
