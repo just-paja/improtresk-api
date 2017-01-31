24be9636404f35679f49f0e3c08d290cb40a0924
just-paja <just.paja@gmail.com>
2017-01-31 19:33:00 +0100

    Add lector endpoint

--- improtreskApi/rest_router.py
+++ improtreskApi/rest_router.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from api.rest import years
+from api.rest import lectors, years
 from api_textual.rest import news, texts
 
 router = routers.DefaultRouter()
+router.register(r'lectors', lectors.LectorViewSet)
 router.register(r'news', news.NewsViewSet)
 router.register(r'texts', texts.TextViewSet)
 router.register(r'years', years.YearViewSet)
