2e385249485e06fdd3292a13aa4794dd60ebc903
Pavel Žák <just.paja@gmail.com>
2017-02-23 20:25:59 +0100

    Add performer model, admin and rest endpoint

---
+++ api_textual/rest/performers.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers, viewsets
+from api.models import Year
+
+from .. import models
+
+
+class PerformerPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.PerformerPhoto
+        fields = (
+            'image',
+            'desc',
+            'height',
+            'width',
+        )
+
+
+class PerformerSerializer(serializers.ModelSerializer):
+    photos = PerformerPhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Performer
+        fields = (
+            'id',
+            'name',
+            'photos',
+            'slug',
+            'text',
+            'year',
+        )
+
+
+class PerformerViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PerformerSerializer
+
+    def get_queryset(self):
+        year = get_object_or_404(Year, year=self.kwargs.get('year', None))
+        return year.performers
