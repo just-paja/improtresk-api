b6af472137583788c8b8f05ec80e923f03e2fbab
Petr Dlouhý <petr.dlouhy@email.cz>
2017-03-13 00:31:08 +0100

    add createOrder endpoint

--- api/tests/models/test_year.py
+++ api/tests/models/test_year.py
@@ -14,3 +14,15 @@ class YearTest(TestCase):
             year="1234",
         )
         self.assertEqual(str(year), 'Year 1234')
+
+    def test_get_actual_price_level_none(self):
+        year = mommy.make('Year')
+        self.assertEqual(year.get_actual_price_level(), None)
+
+    def test_get_actual_price_level(self):
+        price_level = mommy.make('PriceLevel')
+        year = mommy.make(
+            'Year',
+            price_levels=[price_level],
+        )
+        self.assertEqual(year.get_actual_price_level(), price_level)
