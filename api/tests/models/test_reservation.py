cc6cd8e36bf9ad4dc3723bdbbd90faa982f0ce03
just-paja <just.paja@gmail.com>
2017-04-03 01:39:07 +0200

    Fix price display on Reservation

--- api/tests/models/test_reservation.py
+++ api/tests/models/test_reservation.py
@@ -1,7 +1,7 @@
 """Tests for reservation model."""
-from api.models import PriceLevel, Reservation, Workshop, WorkshopPrice
-
 from django.test import TestCase
+
+from model_mommy import mommy
 
 
 class ReservationTest(TestCase):
@@ -9,8 +9,17 @@ class ReservationTest(TestCase):
 
     def test_string_representation(self):
         """Test that reservation turns to string properly."""
-        workshop = Workshop(name="Foo workshop")
-        price_level = PriceLevel(name="Foo price level")
-        workshop_price = WorkshopPrice(price=123, workshop=workshop, price_level=price_level)
-        entry = Reservation(workshop_price=workshop_price, ends_at="2017-1-1")
-        self.assertEqual(str(entry), 'Foo workshop for 123 ends at 2017-1-1')
+        workshop = mommy.make('api.Workshop', name="Foo workshop")
+        price_level = mommy.make('api.PriceLevel', name="Foo price level")
+        workshop_price = mommy.make(
+            'api.WorkshopPrice',
+            price=123,
+            workshop=workshop,
+            price_level=price_level,
+        )
+        entry = mommy.make(
+            'api.Reservation',
+            workshop_price=workshop_price,
+            ends_at="2017-01-01",
+        )
+        self.assertEqual(str(entry), 'Foo workshop for 123 ends at 2017-01-01')
