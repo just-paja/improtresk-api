42914876a83ffdfc9b98aa0fb46ca5dba13f29ef
just-paja <just.paja@gmail.com>
2017-04-03 01:38:56 +0200

    Simplify Meal Reservation definition

--- api/models/mealReservation.py
+++ api/models/mealReservation.py
@@ -3,9 +3,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from .base import Base
-from .food import Food, Soup
-from .meal import Meal
-from .reservation import Reservation
 
 
 class MealReservation(Base):
@@ -16,23 +13,23 @@ class MealReservation(Base):
         verbose_name_plural = _("Meal reservations")
 
     food = models.ForeignKey(
-        Food,
+        'Food',
         verbose_name=_("Food"),
         null=True,
         blank=True,
     )
     soup = models.ForeignKey(
-        Soup,
+        'Soup',
         verbose_name=_("Soup"),
         null=True,
         blank=True,
     )
     meal = models.ForeignKey(
-        Meal,
+        'Meal',
         verbose_name=_("Meal"),
     )
     reservation = models.ForeignKey(
-        Reservation,
+        'Reservation',
         verbose_name=_("Reservation"),
     )
 
