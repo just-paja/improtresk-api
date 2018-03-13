0c53b2f008b06f1e87a30959334ce1ac4d4e8668
just-paja <just.paja@gmail.com>
2018-03-13 03:46:45 +0100

    Fix pylama

--- api/models/participant.py
+++ api/models/participant.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.contrib import auth
 from django.core import mail
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.template.loader import render_to_string
