616f2f39bc1170403930a62beaff715aaf265884
just-paja <just.paja@gmail.com>
2018-05-04 22:18:47 +0200

    Include no-team in teams stats

--- api_admin/views/participant_teams.py
+++ api_admin/views/participant_teams.py
@@ -1,7 +1,9 @@
-from api.models import Team, Year
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Count
 from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
+
+from api.models import Participant, Team, Year
 
 
 @staff_member_required
@@ -14,12 +16,25 @@ def participant_teams(request, festivalId):
             participant__orders__canceled=False,
         )\
         .annotate(count=Count('participant'))\
-        .order_by('-count')
+        .order_by('-count')\
+        .all()
+
+    data = [team for team in teams]
+    teamless_count = Participant.objects.filter(
+        team=None,
+        orders__year=festivalId,
+        orders__paid=True,
+        orders__canceled=False,
+    ).distinct().count()
+    data.append({
+        'name': _('None'),
+        'count': teamless_count,
+    })
     return render(
         request,
         'stats/participant_teams.html',
         {
             'festival': festival,
-            'teams': teams,
+            'teams': data,
         },
     )
