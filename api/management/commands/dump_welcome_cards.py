d752b5c54f51efd30faf6b1c2dabb8ddddab9a72
just-paja <just.paja@gmail.com>
2018-05-10 04:58:17 +0200

    Hotfix welcome cards dump script

--- api/management/commands/dump_welcome_cards.py
+++ api/management/commands/dump_welcome_cards.py
@@ -3,7 +3,7 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from ...models import Year, Order
+from ...models import Year, Order, Reservation
 
 
 class Command(BaseCommand):
@@ -19,25 +19,29 @@ class Command(BaseCommand):
         data = []
         for order in orders:
             food = []
-            if order.reservation:
-                meals = order.reservation.mealreservation_set.all()
-                for meal in meals:
-                    food.append({
-                        'date': str(meal.meal.date),
-                        'food': meal.food.name if meal.food else None,
-                    })
-                data.append({
-                    'id': order.participant.id,
-                    'participant': order.participant.name,
-                    'workshop': (
-                        order.reservation.workshop_price.workshop.name
-                        if order.reservation.workshop_price else None
-                    ),
-                    'accomodation': (
-                        order.reservation.accomodation.name
-                        if order.reservation.accomodation else None
-                    ),
-                    'food': food,
+            try:
+                reservation = order.reservation
+            except Reservation.DoesNotExist:
+                continue
+
+            meals = reservation.mealreservation_set.all()
+            for meal in meals:
+                food.append({
+                    'date': str(meal.meal.date),
+                    'food': meal.food.name if meal.food else None,
                 })
+            data.append({
+                'id': order.participant.id,
+                'participant': order.participant.name,
+                'workshop': (
+                    reservation.workshop_price.workshop.name
+                    if reservation.workshop_price else None
+                ),
+                'accomodation': (
+                    reservation.accomodation.name
+                    if reservation.accomodation else None
+                ),
+                'food': food,
+            })
         with open('improtresk-welcome-cards-data.json', 'w') as outfile:
             json.dump(data, outfile)
