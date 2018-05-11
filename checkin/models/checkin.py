727ab61bdf31e809173b221da4ef9abb34e82e44
just-paja <just.paja@gmail.com>
2018-05-11 13:37:21 +0200

    Send welcome e-mail

--- checkin/models/checkin.py
+++ checkin/models/checkin.py
@@ -1,7 +1,10 @@
 """Year model."""
 
 from api.fields import format_relation_link
+from django.conf import settings
+from django.core import mail
 from django.db import models
+from django.template.loader import render_to_string
 
 from api.models.base import Base
 
@@ -23,6 +26,19 @@ class Checkin(Base):
             'api_participant',
             self.order.participant.id,
             self.order.participant
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.mail_welcome()
+        super().save(*args, **kwargs)
+
+    def mail_welcome(self):
+        mail.send_mail(
+            'Vítej na Improtřesku',
+            render_to_string('mail/welcome.txt'),
+            settings.EMAIL_SENDER,
+            [self.order.participant.email],
         )
 
     participant_link.short_description = "Participant"
